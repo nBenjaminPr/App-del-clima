203005d984607bf7f09158cb5327cdc5c97a7fc1
Nicolas <nicobenpe@gmail.com>
2022-12-15 19:30:22 -0300

    modificando js

--- clima.js
+++ clima.js
@@ -1,6 +1,5 @@
 const api = {
-    key: '9e122cd782b2d0333f5fe4e7fa192062',
-    url: `https://api.openweathermap.org/data/2.5/weather`
+      // PONER API DEL CLIMA
   }
   
   const card = document.getElementById('card')
