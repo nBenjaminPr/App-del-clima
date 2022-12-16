d930c7b750e9b3958f20e68da41d3ce25517d19b
Nicolas <nicobenpe@gmail.com>
2022-12-15 21:38:29 -0300

    Agregando imagenes, colores, mas adelante cambiar de color la pagina

--- clima.js
+++ clima.js
@@ -1,9 +1,9 @@
 const api = {
-      // PONER API DEL CLIMA
+  key: "a8e0474b95316a11382eda3fc002b2da",
+  url: `https://api.openweathermap.org/data/2.5/weather`
   }
   
   const card = document.getElementById('card')
-  
   const city = document.getElementById('city');
   const date = document.getElementById('date');
   const tempImg = document.getElementById('temp-img');
@@ -11,6 +11,8 @@ const api = {
   const weather = document.getElementById('weather');
   const range = document.getElementById('range');
   
+
+
   function updateImages(data) {
     const temp = toCelsius(data.main.temp);
     let src = 'assets/temp-mid.png';
@@ -30,6 +32,7 @@ const api = {
       city.innerHTML = `${data.name}, ${data.sys.country}`;
       data.innerHTML = (new Date()).toLocaleDateString();
       temp.innerHTML = `${toCelsius(data.main.temp)}c`;
+
       weather.innerHTML = data.weather[0].description;
       range.innerHTML = `${toCelsius(data.main.temp_min)}c / ${toCelsius(data.main.temp_max)}c`;
       updateImages(data);
@@ -51,3 +54,5 @@ const api = {
   const searchform = document.getElementById('search-form');
   const searchbox = document.getElementById('searchbox');
   searchform.addEventListener('submit', onSubmit, true);
+
+
